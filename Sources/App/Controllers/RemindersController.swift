e217585360656aab564155d971f1370d2e349930
Gerson Noboa <gerson.noboa@proekspert.ee>
2019-04-01 13:32:48 +0300

    Added getting reminders

--- Sources/App/Controllers/RemindersController.swift
+++ Sources/App/Controllers/RemindersController.swift
@@ -4,6 +4,8 @@ struct RemindersController: RouteCollection {
     func boot(router: Router) throws {
         let remindersRoute = router.grouped("api", "reminders")
         remindersRoute.post(use: createHandler)
+        remindersRoute.get(use: getAllHandler)
+        remindersRoute.get(Reminder.parameter, use: getSingleHandler)
     }
     
     func createHandler(_ req: Request) throws -> Future<Reminder> {
@@ -11,5 +13,14 @@ struct RemindersController: RouteCollection {
             .flatMap(to: Reminder.self) { reminder in
             return reminder.save(on: req)
         }
+    }
+    
+    func getAllHandler(_ req: Request) throws -> Future<[Reminder]> {
+        return Reminder.query(on: req).all()
+    }
+    
+    func getSingleHandler(_ req: Request) throws -> Future<Reminder> {
+        let user = try req.parameters.next(Reminder.self)
+        return user
     }
 }
